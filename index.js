ca3fd5986f3f61a731afb1fae2157bf704a8aaf7
yhtseng1101 <yhtseng1101@gmail.com>
2020-06-23 15:31:49 -0500

    update navigation

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const menuBtn = document.getElementsByClassName("menuBtn");
 
 function checkUri () {
-    if (location.hash.includes('') || location.hash.includes('#home') || location.hash.includes('#projects')) {
-        console.log('its home or projects');
-        for (let i = 0; i < menuBtn.length; i++) {
-            menuBtn[i].style.color = 'white';
-        }
-    } else {
-        console.log('its about or contact');
+    if (location.hash.includes('#about') || location.hash.includes('#contact')) {
         for (let i = 0; i < menuBtn.length; i++) {
             menuBtn[i].style.color = 'black';
+        }
+    } else {
+        for (let i = 0; i < menuBtn.length; i++) {
+            menuBtn[i].style.color = 'white';
         }
     }
     
