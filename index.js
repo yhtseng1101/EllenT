9a9d0563927b97fc6ef51da1f9a80891f2560205
yhtseng1101 <yhtseng1101@gmail.com>
2020-06-23 15:29:21 -0500

    update navigation

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const menuBtn = document.getElementsByClassName("menuBtn");
 
 function checkUri () {
-    if (location.hash.includes('#home') || location.hash.includes('#projects')) {
+    if (location.hash.includes('') || location.hash.includes('#home') || location.hash.includes('#projects')) {
         console.log('its home or projects');
         for (let i = 0; i < menuBtn.length; i++) {
             menuBtn[i].style.color = 'white';
